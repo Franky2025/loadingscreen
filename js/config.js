3b8b6cdb4e567becad98981cdf337e5fc04ff45c
Franky2025 <30404801+Franky2025@users.noreply.github.com>
2017-08-24 14:53:29 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -44,7 +44,24 @@ Avaiable options:
 
 You can add custom backgrounds
 Just make sure it is at webm VP8 format */
-var l_background = "";
+var l_background =  
+         "background/images/mes14.jpg"	
+        "background/images/mes15.jpg"	
+        "background/images/mes16.jpg"	
+        "background/images/mes18.jpg"	
+        "background/images/mes17.jpg"	
+        "background/images/mes2.jpg"	
+        "background/images/mes3.jpg"	
+        "background/images/mes5.jpg"	
+        "background/images/mes8.jpg"	
+        "background/images/mes7.jpg"	
+        "background/images/mes10.jpg"	
+        "background/images/mes6.jpg"
+        "background/images/mes9.jpg"
+        "background/images/mes11.jpg"	
+        "background/images/mes13.jpg"	
+        "background/images/mes12.jpg"
+    ;
 
 /*(Image-background only)
 Place one or more images
